982971376e625d44d09a4d3b788d6c44189c2a24
Manos Kounelakis <pkounelios@gmail.com>
2019-08-05 09:28:19 +0300

    working draft.Display measurements per month and year

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Measurement } from "src/app/interfaces/measurement";
+import { ChartComponent } from "angular2-chartjs";
+import { Dataset } from "src/app/interfaces/dataset";
+import { MeasurementService } from "src/app/services/measurement.service";
 
 @Component({
   selector: "app-history",
@@ -7,6 +10,7 @@ import { Measurement } from "src/app/interfaces/measurement";
   styleUrls: ["./history.component.scss"]
 })
 export class HistoryComponent implements OnInit {
+  @ViewChild("mainChart") mainChart: ChartComponent;
   measurements: Measurement[];
   years: string[];
   months: string[];
@@ -14,19 +18,33 @@ export class HistoryComponent implements OnInit {
   selectedYear: number;
   selectedMonth: number;
   selectedDay: number;
-
+  chartType = "line";
+  data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Number of measurements",
+        data: []
+      }
+    ]
+  };
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
   filteredData: Measurement[];
-  constructor() {}
+  constructor(private measurementService: MeasurementService) {}
 
   ngOnInit() {
-    let data: Measurement[] = JSON.parse(localStorage.getItem("data"));
-    console.log(data);
-    this.measurements = data;
+    this.measurements = this.measurementService.getMeasurements();
     let years = this.measurements.map(m => {
       return new Date(m.date).getFullYear();
     });
 
-    this.years = this.getUniqueItems(years).sort();
+    this.years = this.measurementService
+      .extractYearsFromMeasurements(this.measurements)
+      .map(String);
+    this.onYearChange({ value: this.years[0] });
   }
 
   onYearChange($event) {
@@ -40,22 +58,95 @@ export class HistoryComponent implements OnInit {
 
   onMonthChange($event) {
     this.selectedMonth = +$event.value;
-    console.log({ month: this.selectedMonth, year: this.selectedYear, $event });
-    const days = this.measurements
-      .filter(m => {
-        let date = new Date(m.date);
-        let year = date.getFullYear();
-        let isSameYear = year == this.selectedYear;
-        let month = date.getMonth() + 1;
-        let isSameMonth = this.selectedMonth == month;
+    // Time to find data for selected month and year
+    const filteredData = this.measurements.filter(record => {
+      let date = new Date(record.date);
+      let recordYear = date.getFullYear();
+      //Months in JS start from 0.So for example
+      // January is 0,February is 1 and so on.
+      let recordMonth = date.getMonth() + 1;
+      return (
+        recordYear == this.selectedYear && recordMonth == this.selectedMonth
+      );
+    });
+    let days = filteredData.map(item => new Date(item.date).getUTCDate());
+    // Clear duplicates and sort
+    days = Array.from(new Set(days)).sort((a, b) => a - b);
+    // This returns a new String object for every item in `days`
+    this.days = days.map(String);
+    this.data.datasets[0].label = `Total measurements`;
+    this.data.labels = days.map(day => `Day : ${day}`);
 
-        return isSameMonth && isSameYear;
-      })
-      .map(e => new Date(e.date).getUTCDate());
-    this.days = this.getUniqueItems(days);
+    // const measurements = [];
+    // this.days.forEach(day => {
+    //   let results = this.getMeasurementPerDate(
+    //     this.selectedYear,
+    //     this.selectedMonth,
+    //     day
+    //   );
+    //   console.log({ results });
+    //   measurements.push(results.length);
+    // });
+    const measurements = this.days.map(day => {
+      let results = this.getMeasurementPerDate(
+        this.selectedYear,
+        this.selectedMonth,
+        day
+      );
+      console.log({ results });
+      return results.length;
+    });
+    this.data.datasets[0].data = measurements;
+    this.mainChart.chart.update();
+    // console.log({ month: this.selectedMonth, year: this.selectedYear, $event });
+    // const days = this.measurements
+    //   .filter(m => {
+    //     let date = new Date(m.date);
+    //     let year = date.getFullYear();
+    //     let isSameYear = year == this.selectedYear;
+    //     let month = date.getMonth() + 1;
+    //     let isSameMonth = this.selectedMonth == month;
+
+    //     return isSameMonth && isSameYear;
+    //   })
+    //   .map(e => new Date(e.date).getUTCDate());
+    // this.days = this.getUniqueItems(days).sort((a, b) => a - b);
+    // this.data.datasets[0].label = `Total measurements for month ${
+    //   this.selectedMonth
+    // } of year ${this.selectedYear}`;
+    // this.data.labels = this.days.map(day => `Day : ${day}`);
+    // const measurements = [];
+    // this.days.forEach(day => {
+    //   measurements.push(
+    //     this.getMeasurementPerDate(this.selectedYear, this.selectedMonth, day)
+    //   );
+    // });
+    // this.data.datasets[0].data = measurements.map(e => e.length);
+    // const dataset = this.generatePerDaySys(measurements);
+    // this.data.datasets = [dataset];
+    // this.generateSelectedMonthSet();
+    // this.mainChart.chart.update();
   }
 
+  getMonthName(month: number) {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    return months[month];
+  }
   onDayChange($event) {
+    return;
     this.selectedDay = +$event.value;
     const data = this.measurements.filter(m => {
       let date = new Date(m.date);
@@ -67,9 +158,57 @@ export class HistoryComponent implements OnInit {
       let isSameDay = this.selectedDay == day;
       return isSameDay && isSameMonth && isSameYear;
     });
-    this.filteredData = data;
+    //this.filteredData = data;
   }
   private getUniqueItems(array: any[]) {
     return Array.from(new Set(array));
+  }
+
+  private getMeasurementPerDate(year, month, day) {
+    return this.measurements.filter(m => {
+      let date = new Date(m.date);
+      let mDay = date.getUTCDate();
+      let mYear = date.getFullYear();
+      let mMonth = date.getMonth() + 1;
+      return month == mMonth && mDay == day && mYear == year;
+    });
+  }
+
+  private generatePerDaySys(measurements: Measurement[]): Dataset {
+    console.log(measurements, typeof measurements);
+    let dataset: Dataset = {
+      label: `Per day`,
+      data: [1, 8, 0]
+    };
+
+    return dataset;
+  }
+
+  private generateSelectedMonthSet(): Dataset {
+    // Filter data for selected month and year
+    let filteredData = this.measurements.filter(m => {
+      let date = new Date(m.date);
+      let year = date.getFullYear();
+      let isSameYear = year == this.selectedYear;
+      let month = date.getMonth() + 1;
+      let isSameMonth = this.selectedMonth == month;
+
+      return isSameMonth && isSameYear;
+    });
+
+    let days = filteredData
+      .map(item => new Date(item.date).getUTCDate())
+      .sort((a, b) => a - b);
+    console.log({ days: Array.from(new Set(days)) });
+    let dataset: Dataset = {
+      label: `No of measurements for ${this.getMonthName(
+        this.selectedMonth - 1
+      )}`,
+      data: []
+    };
+    // dataset.data = filteredData.map(item=>{
+
+    // });
+    return dataset;
   }
 }
---
+++ src/app/services/measurement.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from "@angular/core";
+import { Measurement } from "../interfaces/measurement";
+
+@Injectable({
+  providedIn: "root"
+})
+export class MeasurementService {
+  private _dataKey = "data";
+  constructor() {}
+  getMeasurements(): Measurement[] {
+    let data = JSON.parse(localStorage.getItem(this._dataKey)) || [];
+    return data;
+  }
+
+  submitMeasurement(measurement: Measurement) {
+    let data = [...this.getMeasurements(), measurement];
+    localStorage.setItem(this._dataKey, JSON.stringify(data));
+  }
+
+  extractYearsFromMeasurements(data: Measurement[]): number[] {
+    let years = data.map(i => new Date(i.date).getFullYear());
+    //return Array.from(new Set(years)).sort();
+    return this.getUniqueItems(years).sort(this.numSorter);
+  }
+
+  extractDaysFromMeasurements(data: Measurement[]) {
+    let days = data
+      .map(item => new Date(item.date).getUTCDate())
+      .sort(this.numSorter);
+    return this.getUniqueItems(days);
+  }
+
+  // Calculate average systolic pressure from a set
+  public calculateAvgSys(data: Measurement[]) {
+    return (
+      data.reduce(
+        (accum, measurement) => accum + (measurement.systolic as number),
+        0
+      ) / data.length
+    );
+  }
+
+  public calculateAvgDia(data: Measurement[]) {
+    return (
+      data.reduce(
+        (accum, measurement) => accum + (measurement.diastolic as number),
+        0
+      ) / data.length
+    );
+  }
+  private getUniqueItems(data: any[]) {
+    return Array.from(new Set(data));
+  }
+
+  private numSorter(a: number, b: number) {
+    return a - b;
+  }
+}
